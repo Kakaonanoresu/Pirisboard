675d6c7c0fcf61d038478ab99d7fdc6d683a8493
Kuitcodekt <136946712+kuitcodekt@users.noreply.github.com>
2024-08-18 20:35:00 +0900

    Delete .bash

--- .bash
+++
@@ -1,8 +0,0 @@
-# GitHubリポジトリの作成
-# 例: リポジトリ名は "pirisboard" とする
-# GitHubのウェブサイトで新しいリポジトリを作成する方法もあります
-git init
-git add .
-git commit -m "Initial commit"
-git remote add origin https://github.com/kuitcodekt/Pirisboard/
-git push -u origin main
